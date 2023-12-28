56f88119e753bbbdab701ff143327729f107b736
tewodros desta <teddydesta2021@gmail.com>
2023-12-29 01:52:59 +0300

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'http://localhost:3001',
+  cors: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
@@ -45,8 +45,8 @@ app.post('/create-payment', async(req, res)=> {
 })
 
 
-server.listen(8080, ()=> {
-  console.log('server running at port', 8080)
+server.listen(8000, ()=> {
+  console.log('server running at port', 8000)
 })
 
 app.set('socketio', io);
